a12b17fe8bcc42bd9b5aeac683052772b29cb638
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-02 16:58:45 -0300

    Atividade

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var msgGlobal;
 function conferirMensagemWhatsApp() {
   var nome = document.getElementById("nome").value;
   var mensagem = document.getElementById("mensagem").value;
+  var confDiv = document.getElementById("rightDiv");
 
   nomeGlobal = nome;
   msgGlobal = mensagem;
 
+  confDiv.style.display = "flex";
   document.getElementById("confNome").textContent = nome;
   document.getElementById("confMsg").textContent = mensagem;
 }
